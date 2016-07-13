adab7963187a4cd6d4dce75be831ee2c230aae63
Viktoriia <babenko_v_v@ukr.net>
2016-07-13 18:10:27 +0300

    static

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
-	// go js
-
+	$('.carusel').slick({
+  dots: false,
+  infinite: true,
+	// autoplay: true,
+  speed: 300,
+  slidesToShow: 1,
+	prevArrow: '.prev',
+  nextArrow: '.next',
+	});
+	$('.products__conteiner').slick({
+  dots: false,
+  infinite: true,
+	// autoplay: true,
+  speed: 300,
+  slidesToShow: 4,
+	prevArrow: '.products__prev',
+  nextArrow: '.products__next',
+	});
+	$('.style__slides').slick({
+  dots: false,
+  infinite: true,
+	arrows: false,
+	autoplay: true,
+	speed: 500,
+  slidesToShow: 1,
+  fade: true,
+  cssEase: 'linear'
+	});
 });
